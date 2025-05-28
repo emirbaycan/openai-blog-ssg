ec2d7ec3b674a24fcde6b37eb942e56b603e8061
kalinux0 <emir-baycan@hotmail.com>
2025-05-29 01:42:07 +0300

    fix permission error

--- ai/entrypoint.sh
+++ ai/entrypoint.sh
@@ -5,6 +5,6 @@ uvicorn src.ai_api:app --host 0.0.0.0 --port 5000 &
 while true
 do
   sleep 86400
-  echo "[CRON] $(date): Günlük AI blog başlığı işleniyor..."
+  echo "[CRON] $(date): processing blog title"
   python ai/scripts/dispatch_one_job.py
 done
